93d2f0832baa39933143cb216f5f032ec7f8b7b4
UTCBisama <lam241230773@lms.utc.edu.vn>
2026-03-10 12:48:52 +0700

    Add SQL create tables

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -11,11 +11,36 @@ email_customer VARCHAR(50) UNIQUE
 
 CREATE TABLE Employee(
 id_employee VARCHAR(15) PRIMARY KEY,
-
+name_employee VARCHAR(50) NOT NULL,
+phone_employee VARCHAR(10) UNIQUE NOT NULL,
+position_employee VARCHAR(20) NOT NULL,
+hiredate_employee VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE Product(
 id_product VARCHAR(15) PRIMARY KEY,
-
+price VARCHAR(15) NOT NULL,
+name_product VARCHAR(30) NOT NULL,
+brand VARCHAR(15) NOT NULL,
+color VARCHAR(10) NOT NULL,
+stock_quantity VARCHAR(10) NOT NULL,
+VIN VARCHAR(20) UNIQUE NOT NULL,
+prodution_year VARCHAR(5) NOT NULL
 );
 
+CREATE TABLE Invoice(
+id_invoice VARCHAR(15) PRIMARY KEY,
+date_invoice VARCHAR(20) NOT NULL,
+total_amount_invoice VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE Invoice_Detail(
+price_detail VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE Warranty(
+id_warranty VARCHAR(15) PRIMARY KEY,
+start_date_warranty VARCHAR(20) NOT NULL,
+end_date_warranty VARCHAR(20) NOT NULL,
+status_warranty VARCHAR(20) NOT NULL
+);
