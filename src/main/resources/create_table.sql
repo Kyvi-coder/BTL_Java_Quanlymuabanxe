f66834f9a87f7c2dc6d3bc689e8290c6a8c6c224
UTCBisama <lam241230773@lms.utc.edu.vn>
2026-03-10 16:50:34 +0700

    Update create_table.sql

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -11,7 +11,7 @@ email_customer VARCHAR(50) UNIQUE
 
 CREATE TABLE Employee(
 id_employee VARCHAR(15) PRIMARY KEY,
-name_employee VARCHAR(50) NOT NULL,
+name_employee NVARCHAR(50) NOT NULL,
 phone_employee VARCHAR(10) UNIQUE NOT NULL,
 position_employee VARCHAR(20) NOT NULL,
 hiredate_employee VARCHAR(15) NOT NULL
@@ -20,9 +20,9 @@ hiredate_employee VARCHAR(15) NOT NULL
 CREATE TABLE Product(
 id_product VARCHAR(15) PRIMARY KEY,
 price_product VARCHAR(15) NOT NULL,
-name_product VARCHAR(30) NOT NULL,
-brand_product VARCHAR(15) NOT NULL,
-color_product VARCHAR(10) NOT NULL,
+name_product NVARCHAR(30) NOT NULL,
+brand_product NVARCHAR(15) NOT NULL,
+color_product NVARCHAR(10) NOT NULL,
 stock_quantity_product VARCHAR(10) NOT NULL,
 VIN_product VARCHAR(20) UNIQUE NOT NULL,
 prodution_year_product VARCHAR(5) NOT NULL
@@ -42,5 +42,5 @@ CREATE TABLE Warranty(
 id_warranty VARCHAR(15) PRIMARY KEY,
 start_date_warranty VARCHAR(20) NOT NULL,
 end_date_warranty VARCHAR(20) NOT NULL,
-status_warranty VARCHAR(20) NOT NULL
+status_warranty NVARCHAR(20) NOT NULL
 );
